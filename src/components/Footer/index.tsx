1fa973c2ee6397ef3918de2956a51378c7d5fe4a
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-07-01 19:41:32 -0300

    base footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <span>TESTE</span>
+    </footer>
+  );
+};
+
+export default Footer;
