b03485362c04c63754fd2a9ff35265c30bd22aeb
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-07-01 19:41:42 -0300

    import footer

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 /** Componentes */
 import Header from "../../components/Header";
 import Body from "../../components/Body";
+import Footer from "../../components/Footer";
 
 import { Container } from "./styles";
 
