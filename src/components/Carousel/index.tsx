cf2e24232535e793fb2114ba8b4ea026afc3947b
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-07-13 13:05:27 -0300

    fix carousel; adição do <Link to.. /> ao clicar na imagem

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Container,
   DivCarousel,
@@ -12,56 +12,39 @@ import {
   RightArrow,
 } from "./styles";
 
+import { Link } from "react-router-dom";
+
+import useAxios from "../../hooks/useAxios";
+
+interface Empreendimentos {
+  id: number;
+  nome: string;
+  descricao_curta: string;
+  descricao: string;
+  endereco: string;
+  banner: string;
+  poster: string;
+}
+
 const MyCarousel = () => {
-  const items = [
-    {
-      src:
-        "https://i.pinimg.com/originals/c0/e0/fb/c0e0fbd3ebbd368d5b83e69c61873802.jpg",
-      altText: "Grand Village",
-      caption: "Pronto para morar",
-      comodos: "Quarto e sala",
-    },
-    {
-      src:
-        "https://i1.wp.com/livewallpaper.info//wp-content/uploads/2016/12/1920x1080-wallpaper-High-Resolution-Download3.jpg?ssl=1",
-      altText: "LIV",
-      caption: "Pronto para morar",
-      comodos: "Apartamento completo",
-    },
-    {
-      src: "https://i.redd.it/u105ro5rg8o31.jpg",
-      altText: "Tuin",
-      caption: "Pronto para morar",
-      comodos: "Quarto e sala",
-    },
-    {
-      src:
-        "https://wallpaperhd.wiki/wp-content/uploads/wallpapers-1920x1080-bird-silhouette-vector-134154-1920x1080.jpg",
-      altText: "Hadouken",
-      caption: "Pronto para morar",
-      comodos: "Galpão",
-    },
-    {
-      src:
-        "https://i.pinimg.com/originals/8c/c3/d3/8cc3d3b0c19f5ad94cab32de42086688.jpg",
-      altText: "Kamehamehaaaa",
-      caption: "Em construção",
-      comodos: "Empresarial",
-    },
-  ];
+  const { data } = useAxios<Empreendimentos[]>("/show-all");
 
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
 
+  if (!data) {
+    return <p>Carregando...</p>;
+  }
+
   const next = () => {
     if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    const nextIndex = activeIndex === data.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   };
 
   const previous = () => {
     if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    const nextIndex = activeIndex === 0 ? data.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
   };
 
@@ -69,15 +52,16 @@ const MyCarousel = () => {
     if (animating) return;
     setActiveIndex(newIndex);
   };
-
-  const slides = items.map((item) => {
+  const slides = data.map((item) => {
     return (
       <DivCarouselItem
         onExiting={() => setAnimating(true)}
         onExited={() => setAnimating(false)}
-        key={item.src}
+        key={item.id}
       >
-        <img src={item.src} alt={item.altText} />
+        <Link to={`/empreendimentos/detalhes/${data[activeIndex].id}`}>
+          <img src={item.banner} alt={item.nome} onClick={() => {}} />
+        </Link>
       </DivCarouselItem>
     );
   });
@@ -87,18 +71,20 @@ const MyCarousel = () => {
       <FloatDiv>
         <FloatContent>
           <div>
-            <span>{items[activeIndex].caption}</span>
-            <p>{items[activeIndex].altText}</p>
-            <span>{items[activeIndex].comodos}</span>
+            <span>Pronto para morar</span>
+            <p>{data[activeIndex].nome}</p>
+            <span>{data[activeIndex].descricao_curta}</span>
           </div>
           <DivIcons>
             <LeftArrow onClick={() => previous()} />
             <RightArrow onClick={() => next()} />
           </DivIcons>
         </FloatContent>
-        {/* <FloatButton>
-          <span>Clique aqui para conferir</span>
-        </FloatButton> */}
+        <Link to={`/empreendimentos/detalhes/${data[activeIndex].id}`}>
+          <FloatButton>
+            <span>Clique aqui para conferir</span>
+          </FloatButton>
+        </Link>
       </FloatDiv>
 
       <DivCarousel
@@ -163,3 +149,40 @@ export default MyCarousel;
         </div>
       </MyCarousel> */
 }
+
+// const items = [
+//   {
+//     src:
+//       "https://i.pinimg.com/originals/c0/e0/fb/c0e0fbd3ebbd368d5b83e69c61873802.jpg",
+//     altText: "Grand Village",
+//     caption: "Pronto para morar",
+//     comodos: "Quarto e sala",
+//   },
+//   {
+//     src:
+//       "https://i1.wp.com/livewallpaper.info//wp-content/uploads/2016/12/1920x1080-wallpaper-High-Resolution-Download3.jpg?ssl=1",
+//     altText: "LIV",
+//     caption: "Pronto para morar",
+//     comodos: "Apartamento completo",
+//   },
+//   {
+//     src: "https://i.redd.it/u105ro5rg8o31.jpg",
+//     altText: "Tuin",
+//     caption: "Pronto para morar",
+//     comodos: "Quarto e sala",
+//   },
+//   {
+//     src:
+//       "https://wallpaperhd.wiki/wp-content/uploads/wallpapers-1920x1080-bird-silhouette-vector-134154-1920x1080.jpg",
+//     altText: "Hadouken",
+//     caption: "Pronto para morar",
+//     comodos: "Galpão",
+//   },
+//   {
+//     src:
+//       "https://i.pinimg.com/originals/8c/c3/d3/8cc3d3b0c19f5ad94cab32de42086688.jpg",
+//     altText: "Kamehamehaaaa",
+//     caption: "Em construção",
+//     comodos: "Empresarial",
+//   },
+// ];
