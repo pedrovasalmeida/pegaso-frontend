97a5031bfb8b87a0d2bedac9ae4c0f2b9562443a
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-07-15 16:53:54 -0300

    criação do componente 'Remover Empreendimento'

---
+++ src/components/RemoverEmp/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import api from "../../services/api";
+
+import { Container } from "./styles";
+
+interface EmpreendimentoData {
+  id: number;
+  nome: string;
+  descricao_curta: string;
+  descricao: string;
+  endereco: string;
+  banner: string;
+  poster: string;
+}
+
+const RemoveEmp: React.FC = () => {
+  return <Container></Container>;
+};
+
+export default RemoveEmp;
