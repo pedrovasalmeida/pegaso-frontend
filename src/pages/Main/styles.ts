8bcb7d999bbdb09ffcdf337821b34685ecf75c64
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-07-15 16:50:04 -0300

    fix styles

--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
 
-  height: calc(100vh - 81px);
+  height: calc(100vh - 140px);
   width: 100%;
 
   margin: 81px 0 0 0;
