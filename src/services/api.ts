957499d2289ef896c8b2be22e98902050a777e76
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-07-17 03:34:50 -0300

    troca da baseURL(link do heroku para localhost:3333)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
+/** https://desolate-falls-81734.herokuapp.com */
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: 'http://localhost:3333',
 });
 
 export default api;
