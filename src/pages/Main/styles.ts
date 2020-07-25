30bd95e61d60610759ebd44cc8aff985e9da046a
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-07-25 05:17:27 -0300

    fix algumas propriedades

--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -1,11 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
 
-  height: calc(100vh - 140px);
-  width: 100%;
-
-  margin: 81px 0 0 0;
+  height: 100vh;
+  width: 100vw;
 `;
