38ed77a3e143fc79f99f1f38746156e8799083e7
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-07-27 07:12:34 -0300

    fix no retorno do hook

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -2,10 +2,17 @@ import useSWR from 'swr';
 import api from '../services/api';
 
 const fetcher = async (url: string) =>
-  await api.get(url).then((res) => res.data);
+  await api
+    .get(url)
+    .then((res) => res.data)
+    .catch((err) => err);
 
-export default function UseAxios<Data = any>(url: string) {
-  const { data, error } = useSWR<Data>(url, fetcher);
+export default function UseAxios(url: string) {
+  const { data, error } = useSWR(url, fetcher);
 
-  return { data, error };
+  return {
+    results: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
 }
