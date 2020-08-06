0f076177e6ad2b4e014f2c78f815895f7e758950
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-08-06 16:20:08 -0300

    fix; agora renderiza o componente correto de acordo com o tamanho da tela

--- src/components/Infos/index.tsx
+++ src/components/Infos/index.tsx
@@ -15,68 +15,89 @@ import {
   DivImagemTipo,
 } from './styles';
 
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+
+import InfosMobile from '../InfosMobile';
+
 const Infos: React.FC = () => {
+  const { width, height } = useWindowDimensions();
+
   return (
-    <Container>
-      <DivInfos>
-        <DivTitle animate={{ x: 100 }} transition={{ duration: 0.5 }}>
-          <Title>Quem somos</Title>
-        </DivTitle>
+    <>
+      {width < 1500 ? (
+        <InfosMobile />
+      ) : (
+        <Container>
+          <DivInfos>
+            <DivTitle animate={{ x: 100 }} transition={{ duration: 0.5 }}>
+              <Title>Quem somos</Title>
+            </DivTitle>
 
-        <DivDescription animate={{ x: 100 }} transition={{ duration: 0.7 }}>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            tempus euismod laoreet. Integer cursus nulla id lobortis tincidunt.
-            Nulla facilisi. Proin et justo nisi. Morbi est magna, vehicula sed
-            mattis eget, volutpat eu massa. Praesent sed laoreet dolor, vel
-            pellentesque purus. Vivamus at velit in ipsum sollicitudin porta
-            vitae vel eros. Sed porta mauris vel nunc varius venenatis.
-            Pellentesque efficitur rhoncus egestas. In hac habitasse platea
-            dictumst. Nulla eu lacus nec sem porta rutrum. Vestibulum vitae
-            magna sagittis, imperdiet purus sed, pharetra urna. Mauris quis
-            sagittis arcu. Cras quis lectus rutrum erat posuere posuere. Morbi
-            fringilla ante at justo aliquam viverra. Aenean convallis erat
-            fringilla metus fermentum commodo.
-          </Text>
-        </DivDescription>
+            <DivDescription animate={{ x: 100 }} transition={{ duration: 0.7 }}>
+              <Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                tempus euismod laoreet. Integer cursus nulla id lobortis
+                tincidunt. Nulla facilisi. Proin et justo nisi. Morbi est magna,
+                vehicula sed mattis eget, volutpat eu massa. Praesent sed
+                laoreet dolor, vel pellentesque purus. Vivamus at velit in ipsum
+                sollicitudin porta vitae vel eros. Sed porta mauris vel nunc
+                varius venenatis. Pellentesque efficitur rhoncus egestas. In hac
+                habitasse platea dictumst. Nulla eu lacus nec sem porta rutrum.
+                Vestibulum vitae magna sagittis, imperdiet purus sed, pharetra
+                urna. Mauris quis sagittis arcu. Cras quis lectus rutrum erat
+                posuere posuere. Morbi fringilla ante at justo aliquam viverra.
+                Aenean convallis erat fringilla metus fermentum commodo.
+              </Text>
+            </DivDescription>
 
-        <DivCategorias>
-          <SubDivCategorias>
-            <DivTitleMotion animate={{ x: 100 }} transition={{ duration: 0.8 }}>
-              <Title>Qualificações</Title>
-            </DivTitleMotion>
+            <DivCategorias>
+              <SubDivCategorias>
+                <DivTitleMotion
+                  animate={{ x: 100 }}
+                  transition={{ duration: 0.8 }}
+                >
+                  <Title>Qualificações</Title>
+                </DivTitleMotion>
 
-            <Categoria animate={{ x: 100 }} transition={{ duration: 1 }}>
-              <p>Genki-dama Especial</p>
+                <Categoria animate={{ x: 100 }} transition={{ duration: 1 }}>
+                  <p>Genki-dama Especial</p>
 
-              <span>
-                Premiada 12 vezes como a maior Genki-dama já feita em todo o
-                Universo 7.
-              </span>
-            </Categoria>
+                  <span>
+                    Premiada 12 vezes como a maior Genki-dama já feita em todo o
+                    Universo 7.
+                  </span>
+                </Categoria>
 
-            <Separator animate={{ x: 100 }} transition={{ duration: 1.1 }} />
+                <Separator
+                  animate={{ x: 100 }}
+                  transition={{ duration: 1.1 }}
+                />
 
-            <Categoria animate={{ x: 100 }} transition={{ duration: 1.1 }}>
-              <p>Kamehameha Mais Forte</p>
+                <Categoria animate={{ x: 100 }} transition={{ duration: 1.1 }}>
+                  <p>Kamehameha Mais Forte</p>
 
-              <span>O Kamehamehá mais impactante do mercado.</span>
-            </Categoria>
+                  <span>O Kamehamehá mais impactante do mercado.</span>
+                </Categoria>
 
-            <Separator animate={{ x: 100 }} transition={{ duration: 1.2 }} />
+                <Separator
+                  animate={{ x: 100 }}
+                  transition={{ duration: 1.2 }}
+                />
 
-            <Categoria animate={{ x: 100 }} transition={{ duration: 1.2 }}>
-              <p>Teleporte</p>
+                <Categoria animate={{ x: 100 }} transition={{ duration: 1.2 }}>
+                  <p>Teleporte</p>
 
-              <span>
-                Teleporta-se para qualquer ponto no Universo 7 onde há Ki.
-              </span>
-            </Categoria>
-          </SubDivCategorias>
-        </DivCategorias>
-      </DivInfos>
-      <DivImagemTipo animate={{ x: -50 }} transition={{ duration: 1.3 }} />
-    </Container>
+                  <span>
+                    Teleporta-se para qualquer ponto no Universo 7 onde há Ki.
+                  </span>
+                </Categoria>
+              </SubDivCategorias>
+            </DivCategorias>
+          </DivInfos>
+          <DivImagemTipo animate={{ x: -50 }} transition={{ duration: 1.3 }} />
+        </Container>
+      )}
+    </>
   );
 };
 
