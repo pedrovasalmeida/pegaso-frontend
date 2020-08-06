abe39633cb32876edfc2be707ea081ebfef9518a
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-08-06 17:20:38 -0300

    fix; agora renderiza o componente correto de acordo com o tamanho da tela

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -20,6 +20,10 @@ import {
   DivStateMessages,
 } from './styles';
 
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+
+import ContatoMobile from '../ContatoMobile';
+
 const Contato: React.FC = () => {
   const [inputNome, setInputNome] = useState('');
   const [inputEmail, setInputEmail] = useState('');
@@ -33,6 +37,8 @@ const Contato: React.FC = () => {
   const [isSended, setIsSended] = useState(false);
   /** se o email foi enviado ou não */
   const [couldSend, setCouldSend] = useState(true);
+
+  const { width, height } = useWindowDimensions();
 
   const handleInputNome = useCallback((value: string) => {
     setInputNome(value);
@@ -164,145 +170,151 @@ const Contato: React.FC = () => {
   );
 
   return (
-    <Container>
-      <DivDireita>
-        <Titulo animate={{ x: 100 }} transition={{ duration: 0.3 }}>
-          Informações
-        </Titulo>
-        <Texto animate={{ x: 100 }} transition={{ duration: 0.5 }}>
-          Para qualquer informação, dúvida ou comentário, por favor ligue: (11)
-          3456-7890. Ou preencha o fomulário:
-        </Texto>
+    <>
+      {width < 1280 ? (
+        <ContatoMobile />
+      ) : (
+        <Container>
+          <DivDireita>
+            <Titulo animate={{ x: 100 }} transition={{ duration: 0.3 }}>
+              Informações
+            </Titulo>
+            <Texto animate={{ x: 100 }} transition={{ duration: 0.5 }}>
+              Para qualquer informação, dúvida ou comentário, por favor ligue:
+              (11) 3456-7890. Ou preencha o fomulário:
+            </Texto>
 
-        <Form>
-          <Input
-            type="text"
-            placeholder={'Nome'}
-            value={inputNome}
-            onChange={(e) => handleInputNome(e.target.value)}
-            animate={{ x: 100 }}
-            transition={{ duration: 0.7 }}
-            required
-          />
+            <Form>
+              <Input
+                type="text"
+                placeholder={'Nome'}
+                value={inputNome}
+                onChange={(e) => handleInputNome(e.target.value)}
+                animate={{ x: 100 }}
+                transition={{ duration: 0.7 }}
+                required
+              />
 
-          <Input
-            type="email"
-            animate={{ x: 100 }}
-            transition={{ duration: 0.9 }}
-            placeholder={'E-mail'}
-            value={inputEmail}
-            onChange={(e) => handleInputEmail(e.target.value)}
-            required
-          />
+              <Input
+                type="email"
+                animate={{ x: 100 }}
+                transition={{ duration: 0.9 }}
+                placeholder={'E-mail'}
+                value={inputEmail}
+                onChange={(e) => handleInputEmail(e.target.value)}
+                required
+              />
 
-          <Input
-            type="number"
-            minLength={10}
-            pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}"
-            animate={{ x: 100 }}
-            transition={{ duration: 1.1 }}
-            placeholder={'Contato (Tel/Cel)'}
-            value={inputContato}
-            onChange={(e) => handleInputContato(e.target.value)}
-            required
-          />
+              <Input
+                type="number"
+                minLength={10}
+                pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}"
+                animate={{ x: 100 }}
+                transition={{ duration: 1.1 }}
+                placeholder={'Contato (Tel/Cel)'}
+                value={inputContato}
+                onChange={(e) => handleInputContato(e.target.value)}
+                required
+              />
 
-          <InputMessage
-            animate={{ x: 100 }}
-            minLength={10}
-            transition={{ duration: 1.3 }}
-            placeholder={'Mensagem'}
-            value={inputMensagem}
-            onChange={(e) => handleInputMensagem(e.target.value)}
-            required
-          />
+              <InputMessage
+                animate={{ x: 100 }}
+                minLength={10}
+                transition={{ duration: 1.3 }}
+                placeholder={'Mensagem'}
+                value={inputMensagem}
+                onChange={(e) => handleInputMensagem(e.target.value)}
+                required
+              />
 
-          <Button
-            disabled={!couldSend}
-            animate={{ x: 100 }}
-            transition={{ duration: 1.5 }}
-            type="submit"
-            onClick={(e) =>
-              handleSendEmail(
-                e,
-                inputNome,
-                inputEmail,
-                inputContato,
-                inputMensagem,
-              )
-            }
-          >
-            <SendIcon />
-            Enviar
-          </Button>
-        </Form>
-      </DivDireita>
+              <Button
+                disabled={!couldSend}
+                animate={{ x: 100 }}
+                transition={{ duration: 1.5 }}
+                type="submit"
+                onClick={(e) =>
+                  handleSendEmail(
+                    e,
+                    inputNome,
+                    inputEmail,
+                    inputContato,
+                    inputMensagem,
+                  )
+                }
+              >
+                <SendIcon />
+                Enviar
+              </Button>
+            </Form>
+          </DivDireita>
 
-      <DivEsquerda>
-        <Titulo animate={{ x: -100 }} transition={{ duration: 0.3 }}>
-          Endereço
-        </Titulo>
+          <DivEsquerda>
+            <Titulo animate={{ x: -100 }} transition={{ duration: 0.3 }}>
+              Endereço
+            </Titulo>
 
-        <Texto animate={{ x: -100 }} transition={{ duration: 0.5 }}>
-          Rua José Antônio Tourinho Dantas, 2410, Cond. Caminho das Águas, Praia
-          do Flamengo, Salvador-BA
-        </Texto>
+            <Texto animate={{ x: -100 }} transition={{ duration: 0.5 }}>
+              Rua José Antônio Tourinho Dantas, 2410, Cond. Caminho das Águas,
+              Praia do Flamengo, Salvador-BA
+            </Texto>
 
-        <Texto animate={{ x: -100 }} transition={{ duration: 0.7 }}>
-          CEP: 35950-000
-        </Texto>
+            <Texto animate={{ x: -100 }} transition={{ duration: 0.7 }}>
+              CEP: 35950-000
+            </Texto>
 
-        <WhiteSpace />
-        <Titulo animate={{ x: -100 }} transition={{ duration: 0.9 }}>
-          Contato
-        </Titulo>
+            <WhiteSpace />
+            <Titulo animate={{ x: -100 }} transition={{ duration: 0.9 }}>
+              Contato
+            </Titulo>
 
-        <Texto animate={{ x: -100 }} transition={{ duration: 1.1 }}>
-          Tel: (11) 3456-7890
-        </Texto>
-        <Texto animate={{ x: -100 }} transition={{ duration: 1.3 }}>
-          Fax: (11) 3456-7890
-        </Texto>
-        <Texto animate={{ x: -100 }} transition={{ duration: 1.5 }}>
-          WhatsApp: (11) 93456-7890
-        </Texto>
+            <Texto animate={{ x: -100 }} transition={{ duration: 1.1 }}>
+              Tel: (11) 3456-7890
+            </Texto>
+            <Texto animate={{ x: -100 }} transition={{ duration: 1.3 }}>
+              Fax: (11) 3456-7890
+            </Texto>
+            <Texto animate={{ x: -100 }} transition={{ duration: 1.5 }}>
+              WhatsApp: (11) 93456-7890
+            </Texto>
 
-        <WhiteSpace />
-        <Texto animate={{ x: -100 }} transition={{ duration: 1.7 }}>
-          comercial@pegaso.com.br
-        </Texto>
-        <Texto animate={{ x: -100 }} transition={{ duration: 1.9 }}>
-          parceiros@pegaso.com.br
-        </Texto>
-        <Texto animate={{ x: -100 }} transition={{ duration: 2.1 }}>
-          vagas@pegaso.com.br
-        </Texto>
+            <WhiteSpace />
+            <Texto animate={{ x: -100 }} transition={{ duration: 1.7 }}>
+              comercial@pegaso.com.br
+            </Texto>
+            <Texto animate={{ x: -100 }} transition={{ duration: 1.9 }}>
+              parceiros@pegaso.com.br
+            </Texto>
+            <Texto animate={{ x: -100 }} transition={{ duration: 2.1 }}>
+              vagas@pegaso.com.br
+            </Texto>
 
-        <DivStateMessages>
-          {isLoading && (
-            <LoadingMessage>
-              <span>Carregando...</span>
-            </LoadingMessage>
-          )}
+            <DivStateMessages>
+              {isLoading && (
+                <LoadingMessage>
+                  <span>Carregando...</span>
+                </LoadingMessage>
+              )}
 
-          {isError && (
-            <ErrorMessage>
-              Algo deu errado. <br />
-              Por favor, tente novamente
-              <br /> em 5 segundos!
-            </ErrorMessage>
-          )}
+              {isError && (
+                <ErrorMessage>
+                  Algo deu errado. <br />
+                  Por favor, tente novamente
+                  <br /> em 5 segundos!
+                </ErrorMessage>
+              )}
 
-          {isSended && (
-            <SucessMessage>
-              E-mail enviado com sucesso.
-              <br />
-              Em breve, entraremos em contato!
-            </SucessMessage>
-          )}
-        </DivStateMessages>
-      </DivEsquerda>
-    </Container>
+              {isSended && (
+                <SucessMessage>
+                  E-mail enviado com sucesso.
+                  <br />
+                  Em breve, entraremos em contato!
+                </SucessMessage>
+              )}
+            </DivStateMessages>
+          </DivEsquerda>
+        </Container>
+      )}
+    </>
   );
 };
 
