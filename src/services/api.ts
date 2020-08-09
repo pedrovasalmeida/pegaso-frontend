28146b5f3ef63c0a53dff7c39d5cbf50004162d4
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-08-09 06:57:47 -0300

    fix back-end-heroku link

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,10 +3,10 @@ import axios from 'axios';
 /** https://desolate-falls-81734.herokuapp.com */
 
 const local = 'http://localhost:3333';
-// const heroku = 'https://desolate-falls-81734.herokuapp.com';
+const heroku = 'https://agile-castle-69221.herokuapp.com';
 
 const api = axios.create({
-  baseURL: local,
+  baseURL: heroku,
 });
 
 export default api;
