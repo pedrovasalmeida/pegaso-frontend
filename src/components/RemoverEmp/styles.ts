0f76a9bfcc07ab66dc6ab31ad8edc97262724b0d
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-08-18 18:34:40 -0300

    fixing responsivity

--- src/components/RemoverEmp/styles.ts
+++ src/components/RemoverEmp/styles.ts
@@ -12,6 +12,11 @@ export const Container = styled.div`
 export const Lista = styled.div`
   display: flex;
   flex-wrap: wrap;
+  margin: 0 0 80px 0;
+
+  @media (max-width: 1256px) {
+    flex-direction: column;
+  }
 `;
 
 export const Item = styled.div`
@@ -26,6 +31,10 @@ export const Item = styled.div`
 
   border: 1px solid rgba(0, 0, 0, 0.2);
   border-radius: 16px;
+
+  @media (max-width: 1256px) {
+    width: 90%;
+  }
 `;
 
 export const ArrowIcon = styled(AiOutlineDelete)`
@@ -62,6 +71,7 @@ export const Avatar = styled.img`
 export const Data = styled.div`
   display: flex;
   flex-direction: column;
+  width: 50vw;
 `;
 
 export const Nome = styled.span`
@@ -70,12 +80,15 @@ export const Nome = styled.span`
   text-overflow: ellipsis;
   overflow: hidden;
 
-  line-clamp: 2;
-
   width: 250px;
 
   &:first-child {
     font-weight: bold;
+  }
+
+  @media (max-width: 414px) {
+    line-clamp: 1;
+    max-width: 100%;
   }
 `;
 
@@ -86,8 +99,19 @@ export const Descricao = styled.span`
   overflow: hidden;
 
   line-clamp: 2;
+  max-width: 25vw;
 
-  width: 250px;
+  @media (max-width: 1639px) {
+    max-width: 50%;
+  }
+
+  @media (max-width: 1555px) {
+    max-width: 40%;
+  }
+
+  @media (max-width: 1256px) {
+    max-width: 80%;
+  }
 `;
 
 export const Modal = styled.div`
@@ -111,6 +135,28 @@ export const Modal = styled.div`
   filter: drop-shadow(-4px 4px 8px ${shade(0.6, '#324286')});
 
   z-index: 5;
+
+  @media (max-width: 414px) {
+    top: 10vh;
+    left: 5vw;
+
+    width: 350px;
+    min-height: 150px;
+    height: auto;
+
+    padding: 8px;
+  }
+
+  @media (max-width: 320px) {
+    top: 10vh;
+    left: 5vw;
+
+    width: 310px;
+    min-height: 150px;
+    height: auto;
+
+    padding: 8px;
+  }
 `;
 
 export const CloseIcon = styled(TiDelete)`
@@ -163,4 +209,26 @@ export const InfoModal = styled.div`
   filter: drop-shadow(-4px 4px 8px ${shade(0.6, '#324286')});
 
   z-index: 5;
+
+  @media (max-width: 414px) {
+    top: 10vh;
+    left: 5vw;
+
+    width: 350px;
+    min-height: 150px;
+    height: auto;
+
+    padding: 8px;
+  }
+
+  @media (max-width: 320px) {
+    top: 10vh;
+    left: 5vw;
+
+    width: 310px;
+    min-height: 150px;
+    height: auto;
+
+    padding: 8px;
+  }
 `;
