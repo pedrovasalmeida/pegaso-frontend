d0777921f870bd918181966883a52c6b0bb149c6
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2020-08-28 15:41:03 -0300

    updating baseURL to heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,7 @@ const local = 'http://localhost:3333';
 const heroku = 'https://withpostgres.herokuapp.com';
 
 const api = axios.create({
-  baseURL: local,
+  baseURL: heroku,
 });
 
 export default api;
