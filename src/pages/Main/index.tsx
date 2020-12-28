9d82bf8d378468c06014e0f7b708ce7c043a8f72
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-12-28 19:47:04 -0300

    adding a console.log in home page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -6,6 +6,10 @@ import MyCarousel from '../../components/Carousel';
 import { Container } from './styles';
 
 const Main: React.FC = () => {
+  console.log(
+    'Bem-vindo ao console do Chrome. \nVocê está agora no site da Pegaso! \n*********************** \nUse com cuidado! 😉 \n***********************',
+  );
+
   return (
     <Container>
       <MyCarousel />
