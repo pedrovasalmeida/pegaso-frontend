ac9ee87003f0c885d545aaed3398d09bfa4da335
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-01-19 22:23:16 -0300

    adding theme to all app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import Routes from './routes';
 import history from './services/history';
-
-import GlobalStyles from './styles/global';
+import Routes from './routes';
 
 import { AuthProvider } from './context/AuthContext';
 
+import Theme from './styles/Theme';
+
+import GlobalStyles from './styles/global';
+
 const App: React.FC = () => (
-  <>
+  <Theme>
     <AuthProvider>
       <Router history={history}>
         <Routes />
       </Router>
     </AuthProvider>
     <GlobalStyles />
-  </>
+  </Theme>
 );
 
 export default App;
