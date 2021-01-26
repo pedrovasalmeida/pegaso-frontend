8ed3db2ab71f3d09480fc9532cd2171755cc8ca3
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-01-26 17:48:00 -0300

    adding 2 new routes: 'parceiros' and 'serviços prestados'

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,11 +7,12 @@ import Header from './components/Header';
 /** Páginas */
 import Main from './pages/Main';
 import QuemSomos from './pages/QuemSomos';
+import Parceiros from './pages/Parceiros';
 import Empreendimentos from './pages/Empreendimentos';
 import DetalhesEmpreendimento from './pages/DetalhesEmpreendimento';
 import Painel from './pages/Painel';
-import Forgot from './pages/Forgot';
 import Contato from './pages/Contato';
+import Forgot from './pages/Forgot';
 
 const Routes: React.FC = () => {
   return (
@@ -20,11 +21,9 @@ const Routes: React.FC = () => {
       <Switch>
         <Route path="/" exact component={Main} />
         <Route path="/quem-somos" exact component={QuemSomos} />
+        <Route path="/parceiros" exact component={Parceiros} />
         <Route path="/empreendimentos" exact component={Empreendimentos} />
-        <Route
-          path="/empreendimentos/detalhes/:id"
-          component={DetalhesEmpreendimento}
-        />
+        <Route path="/empreendimentos/detalhes/:id" component={DetalhesEmpreendimento} />
         <Route path="/painel" component={Painel} />
         <Route path="/forgot-password" component={Forgot} />
         <Route path="/contato" component={Contato} />
