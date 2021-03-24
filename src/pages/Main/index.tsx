0ec66808d259c3301e69069f3f5dadfceee479d5
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-03-24 17:37:40 -0300

    changing carousel to static image

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,26 +1,13 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { useSwipeable } from 'react-swipeable';
+import React from 'react';
 
-import { Preloader, ThreeDots } from 'react-preloader-icon';
-
+import ReactGA from 'react-ga';
 import Footer from '../../components/Footer';
 
-import api from '../../services/api';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 
-import {
-  Carousel,
-  Container,
-  Imagem,
-  FloatDiv,
-  FloatContent,
-  FloatButton,
-  ErrorMessage,
-  DivIcons,
-  LeftArrow,
-  RightArrow,
-} from './styles';
+import { Container, Imagem, MyLink } from './styles';
+
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 interface Empreendimentos {
   id: number;
@@ -33,136 +20,22 @@ interface Empreendimentos {
 }
 
 const Main: React.FC = () => {
-  const [sliding, setSliding] = useState(0);
-  const [dir] = useState('NEXT');
-  const [loading, setLoading] = useState(true);
-  const [hasData, setHasData] = useState(false);
-  const [results, setResults] = useState<any>(null);
-
-  const getData = async () => {
-    const dataFromLocalStorage = localStorage.getItem('@ProjPegaso:enterpriseData');
-
-    if (dataFromLocalStorage) {
-      setResults(JSON.parse(dataFromLocalStorage));
-      setHasData(true);
-      setLoading(false);
-
-      const response = await api.get('/show-all');
-
-      if (
-        JSON.stringify(response.data) !== dataFromLocalStorage &&
-        response.status === 200
-      ) {
-        localStorage.setItem('@ProjPegaso:enterpriseData', JSON.stringify(response.data));
-        setResults(response.data);
-      }
-    } else {
-      api
-        .get('/show-all')
-        .then(res => {
-          setResults(res.data);
-          localStorage.setItem('@ProjPegaso:enterpriseData', JSON.stringify(res.data));
-          setHasData(true);
-          setLoading(false);
-        })
-        .catch(err => {
-          setHasData(false);
-          setLoading(false);
-        });
-    }
-  };
-
-  const handleNext = useCallback(() => {
-    if (sliding !== results.length - 1) {
-      setSliding(sliding + 1);
-    } else {
-      setSliding(0);
-    }
-  }, [sliding, setSliding, results]);
-
-  const handleBack = useCallback(() => {
-    if (sliding !== 0) {
-      setSliding(sliding - 1);
-    } else {
-      setSliding(results.length - 1);
-    }
-  }, [sliding, setSliding, results]);
-
-  const handlers = useSwipeable({
-    onSwipedLeft: () => handleNext(),
-    onSwipedRight: () => handleBack(),
-    preventDefaultTouchmoveEvent: true,
-    trackMouse: true,
-  });
-
-  useEffect(() => {
-    getData();
-  }, []);
+  const { width } = useWindowDimensions();
 
   return (
     <>
-      {loading ? (
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '100%',
-            height: '95vh',
-          }}
-        >
-          <Preloader
-            use={ThreeDots}
-            size={120}
-            strokeWidth={6}
-            strokeColor="#0e6387"
-            duration={1000}
-          />
-        </div>
+      {width < 800 ? (
+        <Container>
+          <MyLink to="/obras">
+            <Imagem src="https://i.imgur.com/CVtmdgh.png" alt="Pégaso" />
+          </MyLink>
+        </Container>
       ) : (
-        <>
-          {!hasData ? (
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: '100%',
-                height: '95vh',
-              }}
-            >
-              <ErrorMessage>
-                Ainda não possuimos empreendimentos cadastrados!
-              </ErrorMessage>
-            </div>
-          ) : (
-            <Carousel {...handlers} style={{ cursor: 'grab' }}>
-              {results.map(item => (
-                <Container key={item.id} sliding={sliding} dir={dir}>
-                  <Imagem src={item.banner} alt={item.nome} />
-                </Container>
-              ))}
-
-              <FloatDiv>
-                <FloatContent>
-                  <div>
-                    <p>{results[sliding].nome}</p>
-                    <span>{results[sliding].descricao_curta}</span>
-                  </div>
-                  <DivIcons>
-                    <LeftArrow size={50} onClick={() => handleBack()} />
-                    <RightArrow size={50} onClick={() => handleNext()} />
-                  </DivIcons>
-                </FloatContent>
-                <Link to={`/obras/detalhes/${results[sliding].id}`}>
-                  <FloatButton>
-                    <span>Clique aqui para conferir</span>
-                  </FloatButton>
-                </Link>
-              </FloatDiv>
-            </Carousel>
-          )}
-        </>
+        <Container>
+          <MyLink to="/obras">
+            <Imagem src="https://i.imgur.com/BVKrDNR.png" alt="Pégaso" />
+          </MyLink>
+        </Container>
       )}
       <Footer />
     </>
